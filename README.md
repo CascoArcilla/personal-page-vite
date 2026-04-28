aa4ee28b8d6d534c3b76f2c16d5820ea491da0cd
CascoArcilla <normon1230@gmail.com>
2026-04-28 17:11:49 -0600

    Se agregan tres proyectos propios junto y trabajando en optimizar el codigo js para ellos

---
+++ README.md
@@ -0,0 +1,26 @@
+# Pagina personal
+
+Pagina web para mostrar información sobre mi como:
+
+- Información personal
+- Habilidades
+- Experiencia
+- Tecnologías que he usado
+- Proyectos desarrollados
+
+## Características
+
+- Diseño responsivo
+- Modo oscuro
+- Animaciones simples
+
+## Tecnologías
+
+- HTML
+- CSS
+- Vanilla JavaScript
+
+### Comandos útiles
+
+- `npm run dev` -> Inicia el servidor en modo desarrollo
+- `npm run build` -> Compila el proyecto para producción
--- src/sections/projects.js
+++ src/sections/projects.js
@@ -1,5 +1,8 @@
 import '../css/projects.css';
 import { blackJack } from './projects/black-jack.js';
+import { tzServiceWeb } from './projects/tz-service-web.js';
+import { tzAndroid } from './projects/tz-android.js';
+import { sensoLab } from './projects/senso-lab.js';
 
 export function projects() {
     return `
@@ -9,9 +12,9 @@ export function projects() {
     </header>
     <div class="container-projects">
         ${blackJack()}
-        ${blackJack()}
-        ${blackJack()}
-        ${blackJack()}
+        ${tzServiceWeb()}
+        ${tzAndroid()}
+        ${sensoLab()}
     </div>
 </section>
     `;
